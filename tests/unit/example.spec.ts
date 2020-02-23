0cf24d9de933359d2a116e0fd410c432e467e81c
derushio <derushio@gmail.com>
2020-02-23 17:02:12 +0900

    <master>[modified]

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import { quasarApiCustomize } from '../../src/plugins/quasarApiCustomize';
+import { quasarApiCustomize } from '@/plugins/quasarApiCustomize';
 quasarApiCustomize(Vue);
 
 import { shallowMount } from '@vue/test-utils';
